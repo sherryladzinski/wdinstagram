82d14f1701779b859373b3ef1ec7ce04a8349ed8
Sherry Ladzinski <sherryladzinski@gmail.com>
2015-01-11 23:42:19 -0500

    added views

--- wdinstagram_app/app/controllers/entries_controller.rb
+++ wdinstagram_app/app/controllers/entries_controller.rb
@@ -1,5 +1,25 @@
 class EntriesController < ApplicationController
 	def index
+		@entries = Entry.all
 	end
 
+	def show
+	end
+
+	def new
+		@entry = Entry.new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+		@movie.destroy
+	end
 end
--- wdinstagram_app/db/seeds.rb
+++ wdinstagram_app/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@entries = Entry.create([{ author: "Sherry"}, {photo_url: "www.google.com"}, {date_taken: ""}])
